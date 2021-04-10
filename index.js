7b4fc99d1a3d1415fb8b3b6c65597f982fe7368e
Mavis2103 <taquanvku@gmail.com>
2021-04-10 20:52:39 +0700

    fix

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-// Routes
-import postsRoutes from './routes/postsRoute.js';
+import postsRoutes from "./routes/postsRoute.js";
 
 const app = express();
 
@@ -16,13 +15,17 @@ app.use(cors());
 //     res.send('Hello, this is node 🎉');
 // })
 
-const CONNECTION_URL = 'mongodb+srv://Khale:Khale123@cluster0.m08om.mongodb.net/Vigor?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://Khale:Khale123@cluster0.m08om.mongodb.net/Vigor?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
-mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+mongoose.set("useFindAndModify", false);
 
 // User routes
-app.use('/', postsRoutes);
+app.use("/", postsRoutes);

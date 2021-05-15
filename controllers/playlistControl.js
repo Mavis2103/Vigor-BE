a2d5175ecea16240684adf0adf356cc15900fa8f
Mavis2103 <taquanvku@gmail.com>
2021-05-15 15:44:13 +0700

    up new feature post to playlist

--- controllers/playlistControl.js
+++ controllers/playlistControl.js
@@ -1,58 +1,101 @@
-import PlaylistSchema from '../models/playlistDB.js';
+import PlaylistSchema from "../models/playlistDB.js";
+import PostSchema from "../models/postDB.js";
 
 //getPlaylist - GET
-export const getPlaylist = async(req, res) => {
-    try {
-        const playlist = await PlaylistSchema.find()
-            .populate('creator', '_id username profilePicture')
-            // .sort({ createdAt: -1 });
-        if (!playlist) throw Error('No playlist!');
-        res.status(200).json(playlist);
-    } catch (error) {
-        res.status(400).json({ msg: error });
-    }
-}
+export const getPlaylist = async (req, res) => {
+  try {
+    const playlist = await PlaylistSchema.find().populate(
+      "creator",
+      "_id username profilePicture"
+    );
+    // .sort({ createdAt: -1 });
+    if (!playlist) throw Error("No playlist!");
+    res.status(200).json(playlist);
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
+};
 
 // createPlaylist - POST
 export const createPlaylist = (req, res) => {
-    try {
-        // const post = await PostSchema(req.body).save();
-        const { title, songs } = req.body;
-        req.user.password = undefined;
-        const playlist = new PlaylistSchema({
-            title,
-            creator: req.user,
-            songs
-        })
-        playlist.save()
-            .then(result => {
-                res.status(201).json(result);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-            // if (!post) throw Error('Something went wrong!');
-            // res.status(201).json(post);
-    } catch (error) {
-        res.status(409).json({ msg: error });
-    }
-}
+  try {
+    // const post = await PostSchema(req.body).save();
+    const { title, songs } = req.body;
+    req.user.password = undefined;
+    const playlist = new PlaylistSchema({
+      title,
+      creator: req.user,
+      songs,
+    });
+    playlist
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // if (!post) throw Error('Something went wrong!');
+    // res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ msg: error });
+  }
+};
 
 // Delete playlist - REMOVE
 export const deletePlaylist = (req, res) => {
-    PlaylistSchema.findOne({ _id: req.params.playlistId })
-        .populate('creator', '_id')
-        .exec((err, playlist) => {
-            if (err || !playlist) {
-                return res.status(422).json({ error: err })
-            }
-            if (playlist.creator._id.toString() === req.user._id.toString()) {
-                playlist.remove()
-                    .then(result => {
-                        res.json(result)
-                    }).catch(err => {
-                        console.log(err)
-                    })
-            }
-        })
-}
+  PlaylistSchema.findOne({ _id: req.params.playlistId })
+    .populate("creator", "_id")
+    .exec((err, playlist) => {
+      if (err || !playlist) {
+        return res.status(422).json({ error: err });
+      }
+      if (playlist.creator._id.toString() === req.user._id.toString()) {
+        playlist
+          .remove()
+          .then((result) => {
+            res.json(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+};
+
+export const addPostToPlaylist = async (req, res) => {
+  const { id_post, id_playlist } = req.body;
+  try {
+    let playlist = await PlaylistSchema.findOneAndUpdate(
+      { _id: id_playlist },
+      { $push: { songs: id_post } },
+      { new: true }
+    );
+    res.status(200).json(playlist);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+export const viewPostFromPlaylist = async (req, res) => {
+  const { id_playlist } = req.body;
+  try {
+    let playlist = await PlaylistSchema.find({ _id: id_playlist });
+    let posts = await PostSchema.find({ _id: playlist[0].songs });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+export const removePostToPlaylist = async (req, res) => {
+  const { id_post, id_playlist } = req.body;
+  try {
+    let playlist = await PlaylistSchema.findOneAndUpdate(
+      { _id: id_playlist },
+      { $pull: { songs: id_post } },
+      { new: true, multi: true }
+    );
+    res.status(200).json(playlist);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,54 +1,84 @@
-import express from 'express';
-import { login, signUp } from '../controllers/authControl.js';
-import { createPlaylist, deletePlaylist, getPlaylist } from '../controllers/playlistControl.js';
-import { comment, createPost, deleteComment, deletePost, getLikers, getMyPost, getPost, getPostFollow, likePost, unLikePost,ListHashtag, PostWithHashtag } from '../controllers/postControl.js';
-import { followOthers, getUserPost, searchUser, unFollowOthers, updateAvatar } from '../controllers/userControl.js';
-import { requireLogin } from '../middleware/requireLogin.js';
+import express from "express";
+import { login, signUp } from "../controllers/authControl.js";
+import {
+  createPlaylist,
+  deletePlaylist,
+  getPlaylist,
+  addPostToPlaylist,
+  removePostToPlaylist,
+  viewPostFromPlaylist,
+} from "../controllers/playlistControl.js";
+import {
+  comment,
+  createPost,
+  deleteComment,
+  deletePost,
+  getLikers,
+  getMyPost,
+  getPost,
+  getPostFollow,
+  likePost,
+  unLikePost,
+  ListHashtag,
+  PostWithHashtag,
+} from "../controllers/postControl.js";
+import {
+  followOthers,
+  getUserPost,
+  searchUser,
+  unFollowOthers,
+  updateAvatar,
+} from "../controllers/userControl.js";
+import { requireLogin } from "../middleware/requireLogin.js";
 
 const router = express.Router();
 
-router.route('/').get(requireLogin, getPost) //get all posts
-    .post(requireLogin, createPost) //create post
-    .put(requireLogin, comment); //comment
+router
+  .route("/")
+  .get(requireLogin, getPost) //get all posts
+  .post(requireLogin, createPost) //create post
+  .put(requireLogin, comment); //comment
 
 // Get my posts
-router.get('/profile/myPosts', requireLogin, getMyPost);
+router.get("/profile/myPosts", requireLogin, getMyPost);
 // Get other users posts
-router.get('/profile/:username/posts', requireLogin, getUserPost);
+router.get("/profile/:username/posts", requireLogin, getUserPost);
 // Get my following users posts
-router.get('/followingUser', requireLogin, getPostFollow);
+router.get("/followingUser", requireLogin, getPostFollow);
 // Follow other users
-router.put('/follow', requireLogin, followOthers);
+router.put("/follow", requireLogin, followOthers);
 // router.put('/profile/myPosts', requireLogin, )
 // Like post
-router.patch('/like', requireLogin, likePost);
+router.patch("/like", requireLogin, likePost);
 // Get likers
-router.get('/likers', requireLogin, getLikers);
+router.get("/likers", requireLogin, getLikers);
 // Unlike post
-router.patch('/unlike', requireLogin, unLikePost);
+router.patch("/unlike", requireLogin, unLikePost);
 // Delete posts
-router.delete('/:postId', requireLogin, deletePost);
+router.delete("/:postId", requireLogin, deletePost);
 // Delete comment
-router.delete('/deleteComment/:postId/:commentId', requireLogin, deleteComment);
+router.delete("/deleteComment/:postId/:commentId", requireLogin, deleteComment);
 // Unfollow other users
-router.put('/unfollow', requireLogin, unFollowOthers);
+router.put("/unfollow", requireLogin, unFollowOthers);
 // Update profile picture
-router.put('/updateAvatar', requireLogin, updateAvatar);
+router.put("/updateAvatar", requireLogin, updateAvatar);
 // Search user
-router.post('/searchUser', searchUser);
+router.post("/searchUser", searchUser);
 // Get all playlists
-router.get('/getPlaylist', requireLogin, getPlaylist);
+router.get("/getPlaylist", requireLogin, getPlaylist);
 // Create playlist
-router.post('/createPlaylist', requireLogin, createPlaylist);
+router.post("/createPlaylist", requireLogin, createPlaylist);
 // Delete playlist
-router.delete('/deletePlaylist/:playlistId', requireLogin, deletePlaylist);
+router.delete("/deletePlaylist/:playlistId", requireLogin, deletePlaylist);
 // Auth route
-router.post('/register', signUp);
-router.post('/login', login);
-router.get('/hashtag', requireLogin,ListHashtag);
-router.get('/hashtag/:hashtag',requireLogin, PostWithHashtag);
+router.post("/register", signUp);
+router.post("/login", login);
+router.get("/hashtag", requireLogin, ListHashtag);
+router.get("/findPostwithhashtag", requireLogin, PostWithHashtag);
 
-
+router.patch("/addPostToPlaylist", requireLogin, addPostToPlaylist);
+router.patch("/viewPostFromPlaylist", requireLogin, viewPostFromPlaylist);
+router.patch("/removePostToPlaylist", requireLogin, removePostToPlaylist);
 
 //Hashtag (create collection for hashtag)
 //Type (create collection for Type)
